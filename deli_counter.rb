3dbe36023a3d41008325dc57e04440563b522060
Diego Vasquez <botellongo69@outlook.com>
2020-01-24 19:25:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,11 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size === 0 
     puts 'The line is currently empty.'
+  else
+    newStr =  "The line is currently:"
+    katz_deli.each_with_index { |item, index|
+      newStr << " #{index + 1}."
+      newStr << " #{item[index]}"
+    }
   end
 end
