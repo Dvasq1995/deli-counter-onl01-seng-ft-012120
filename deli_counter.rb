a9206d77f695bb0ce7247ceb742aa6f2c99e2a10
Diego Vasquez <botellongo69@outlook.com>
2020-01-24 19:18:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size === 0 
+    puts 'The line is currently empty.'
+  else
+    newStr = 'The line is currently:'
+    i = 0 
+    while i < katz_deli.size
+      newStr << " #{i + 1}."
+      newStr << " #{katz_deli[i]}"
+      counter += 1
+    end
+    newStr
+  end
+end
